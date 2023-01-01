0da64f1c0695c4595c750155e3dc7fe67878e83f
Lili fernandez <lilibetfdez@gmail.com>
2023-01-01 18:56:54 +0100

    added id new route

--- blog/helpers/url-helper.js
+++ blog/helpers/url-helper.js
@@ -11,7 +11,7 @@ const URLS = {
         return `${process.env.NEXT_PUBLIC_URL_API}/articles/${id}`;
     },
     URL_ARTICLES_EDIT: (id) => {
-        return `${process.env.NEXT_PUBLIC_URL_API}/articles/${id}`;
+        return `${process.env.NEXT_PUBLIC_URL_API}/getArticle?id=${id}`;
     },
     URL_ARTICLES_CHANGE_STATUS: (id) => {
         return `${process.env.NEXT_PUBLIC_URL_API}/articles/update-status/${id}`;
